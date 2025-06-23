f6ef31f70447bd2460e9c979c22ff78fd7b97cf4
ycbalingasag <ycbalingasag@gmail.com>
2025-06-24 01:23:47 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: '*' },
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit('user-joined', socket.id);
+  });
+
+  socket.on('signal', ({ signal, to }) => {
+    io.to(to).emit('receive-signal', { signal, from: socket.id });
+  });
+
+  socket.on('return-signal', ({ signal }) => {
+    socket.broadcast.emit('receive-return-signal', { signal });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log('Socket.io server running on port 5000');
+});
